5202fb687b6cd8bf87242b80596bfe787793df06
MasterTbo <matjele7@gmail.com>
2019-03-12 09:53:05 +0200

    This is java lingerie catalogue system integrated with html file

--- src/pgdswd/lingerie/LingerieCatalogue.java
+++ src/pgdswd/lingerie/LingerieCatalogue.java
@@ -1,19 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pgdswd.lingerie;
 
-import java.io.File;
 import javax.swing.ImageIcon;
 import java.net.URL; 
 import java.io.IOException;
 
 
 /**
- *
- * @author pgd-swd
+ * Demonstrates the lingerie App
+ * @author matjele7@gmail.com
  */
 public class LingerieCatalogue extends javax.swing.JFrame {
     
@@ -41,45 +35,94 @@ public class LingerieCatalogue extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jLabel1 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jEditorPane1 = new javax.swing.JEditorPane();
+        jepCatalogueDetails = new javax.swing.JEditorPane();
+        jpImageIcon = new javax.swing.JPanel();
+        lblCatalogueImage = new javax.swing.JLabel();
+        jPanel3 = new javax.swing.JPanel();
+        btnCatalogue = new javax.swing.JButton();
+        btnExit = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Lingerie Catalogue");
 
-        jButton1.setText("Exit");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder("Catalogue Details"));
+
+        jScrollPane1.setViewportView(jepCatalogueDetails);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jScrollPane1)
+        );
+
+        jpImageIcon.setBorder(javax.swing.BorderFactory.createTitledBorder("Catalogue Image"));
+
+        javax.swing.GroupLayout jpImageIconLayout = new javax.swing.GroupLayout(jpImageIcon);
+        jpImageIcon.setLayout(jpImageIconLayout);
+        jpImageIconLayout.setHorizontalGroup(
+            jpImageIconLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(lblCatalogueImage, javax.swing.GroupLayout.DEFAULT_SIZE, 127, Short.MAX_VALUE)
+        );
+        jpImageIconLayout.setVerticalGroup(
+            jpImageIconLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(lblCatalogueImage, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 168, Short.MAX_VALUE)
+        );
+
+        jPanel3.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+
+        btnCatalogue.setText("First Catalogue Item");
+        btnCatalogue.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                btnCatalogueActionPerformed(evt);
             }
         });
 
-        jButton2.setText("First Catalogue Item");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        btnExit.setText("Exit");
+        btnExit.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                btnExitActionPerformed(evt);
             }
         });
 
-        jScrollPane1.setViewportView(jEditorPane1);
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(btnCatalogue)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 110, Short.MAX_VALUE)
+                .addComponent(btnExit)
+                .addContainerGap())
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnExit)
+                    .addComponent(btnCatalogue))
+                .addContainerGap())
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+            .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 173, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jButton2)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jButton1))
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 292, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addComponent(jpImageIcon, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -87,63 +130,72 @@ public class LingerieCatalogue extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 144, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 133, Short.MAX_VALUE)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(jButton2))
-                .addContainerGap())
+                        .addComponent(jpImageIcon, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 62, Short.MAX_VALUE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(11, 11, 11))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
     
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    private void btnCatalogueActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCatalogueActionPerformed
         //images/lingerie_00 + list[count] + .png
         ImageIcon imgIcon = createImageIcon("images/" + imgFilesPath[count]);
         //html/00 + list[count] + .html
         URL htlmUrl = createFileUrl("html/" + htmlFilesPath[count]);
         if(imgIcon != null){
           try{
-            jLabel1.setIcon(imgIcon);
+            lblCatalogueImage.setIcon(imgIcon);
+            
             if(htlmUrl != null){
-              jEditorPane1.setPage(htlmUrl);
+              jepCatalogueDetails.setPage(htlmUrl);
             }else{
-              jEditorPane1.setPage(createFileUrl(" "));
+              jepCatalogueDetails.setPage(createFileUrl(" "));
             }
           }catch(IOException ex){
             javax.swing.JOptionPane.showMessageDialog(null, "File not found", "Error", 0);
           }
           
           if(count == 9){
-             jButton2.setText("First Catalogue Item");
+             btnCatalogue.setText("First Catalogue Item");
             count = 0;
           }else{
-              jButton2.setText("Next Catalogue Item");
+              btnCatalogue.setText("Next Catalogue Item");
               ++count;}
         }else{
             javax.swing.JOptionPane.showMessageDialog(null, "No image found", "Error", 0);
         }
-    }//GEN-LAST:event_jButton2ActionPerformed
+    }//GEN-LAST:event_btnCatalogueActionPerformed
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void btnExitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnExitActionPerformed
         System.exit(0);
        
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_btnExitActionPerformed
     
+    /**
+     * A function createImageIcon of the lingerie
+     * @param path a string path to the lingerie icon
+     * @return the lingerie icon
+     */
     private ImageIcon createImageIcon(String path)
     {
         URL imgPath = getClass().getResource(path);
         if(imgPath != null){
             return new ImageIcon(imgPath);
-    }
+        }
         doMsg(path);
         return null;
     }  
 
+    /**
+     * A function createFileUrl of the html file with details of the lingerie
+     * @param path a string path to the html file
+     * @return the lingerie html file
+     */
     private URL createFileUrl(String path){
         URL url = getClass().getResource(path);
         if(url == null){
@@ -195,10 +247,16 @@ public class LingerieCatalogue extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JEditorPane jEditorPane1;
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JButton btnCatalogue;
+    private javax.swing.JButton btnExit;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel3;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JEditorPane jepCatalogueDetails;
+    private javax.swing.JPanel jpImageIcon;
+    private javax.swing.JLabel lblCatalogueImage;
     // End of variables declaration//GEN-END:variables
 }
+
+
+
